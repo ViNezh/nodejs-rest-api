8d7be084c1b129c5ffda97bdd15cf2433ecbaa42
ViNezh <126874232+ViNezh@users.noreply.github.com>
2023-12-19 15:57:41 +0200

    contact verification

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -4,11 +4,14 @@ const { Contact } = require("../../models/contact");
 const add = async (req, res) => {
   const { _id: owner } = req.user;
   const { name } = req.body;
-  const checkName = await Contact.findOne({ name });
-  if (checkName) {
+
+  const existingContact = await Contact.findOne({ name, owner });
+
+  if (existingContact) {
     throw HttpError(409, "Contact with same name already exist");
   }
   const newContact = await Contact.create({ ...req.body, owner });
+
   res.status(201).json(newContact);
 };
 
--- controllers/users/authCurrent.js
+++ controllers/users/authCurrent.js
@@ -1,8 +1,9 @@
 const getCurrent = async (req, res) => {
-  const { name, email } = req.user;
+  const { name, email, subscription } = req.user;
   res.json({
     name,
     email,
+    subscription,
   });
 };
 
